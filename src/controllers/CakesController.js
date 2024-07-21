0d96c553f391a172614d69086385cb4cd37fcad6
Xanderistme <xanderitsme@gmail.com>
2024-07-21 08:57:25 -0500

    Agrega productos dinamicos

--- src/controllers/CakesController.js
+++ src/controllers/CakesController.js
@@ -1,6 +1,14 @@
+import cakes from '@/mocks/cakes.json';
+
 export class CakeController {
-  getCakes = async ({ limit }) => {
-    const response = await fetch("")
-    
+  static getCakes = async ({ limit = 10 }) => {
+    // const response = await fetch("")
+    // throw new Error("Cakes'nt")
+
+    if (limit < 1 || limit > 100) {
+      return cakes.slice(0, 10);
+    }
+
+    return cakes.slice(0, limit);
   }
 }
--- src/views/PopularProducts.jsx
+++ src/views/PopularProducts.jsx
@@ -1,65 +1,60 @@
 import { PopularProductCard } from "@/components/PopularProductCard"
+import { CakeController } from "@/controllers/CakesController"
 import "@/ui/views/popular-products.css"
-
-const cakes = [
-  {
-    imgUrl: "/img/pasteles/pastel-moca-con-crema.webp",
-    imgAlt: "imagen del producto",
-    title: "Pastel de moca con crema",
-    description: "Bizcocho suave de café con crema dulce y un toque de chocolate",
-    price: 72,
-    redirectTo: "#",
-  },
-  {
-    imgUrl: "/img/pasteles/pastel-maracuya.webp",
-    imgAlt: "imagen del producto",
-    title: "Pastel de maracuya",
-    description: "Esponjoso bizcocho con relleno y cobertura de crema de fruta",
-    price: 80,
-    redirectTo: "#",
-  },
-  {
-    imgUrl: "/img/pasteles/cheesecake-oreo.webp",
-    imgAlt: "imagen del producto",
-    title: "Cheesecake oreo",
-    description: "Cremoso cheesecake con trozos crujientes de Oreo",
-    price: 86,
-    redirectTo: "#",
-  },
-  {
-    imgUrl: "/img/pasteles/cheesecake-fresa.webp",
-    imgAlt: "imagen del producto",
-    title: "Cheesecake de fresa",
-    description: "Suave cheesecake con cobertura de salsa de fresa",
-    price: 72,
-    redirectTo: "#",
-  }
-]
+import { useEffect, useState } from "react"
 
 export const PopularProducts = () => {
+  const [cakes, setCakes] = useState([])
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    CakeController.getCakes({ limit: 4 })
+      .then((newCakes) => {
+        if (newCakes < 0) {
+          setError(true)
+          return
+        }
+
+        setCakes(newCakes);
+      })
+      .catch((e) => {
+        console.error(e)
+        setError(true)
+      })
+  }, [])
+
   return (
     <section className="popular-products px-8 py-8">
       <header className="max-w-1280 mx-auto py-4">
         <h2 className="titulo-seccion">Productos más pedidos</h2>
       </header>
       <div className="max-w-1280 mx-auto">
-        <ul className="popular-products-container">
-          {
-            cakes.map(cake => (
-              <li className="contents" key={cake.id}>
-                <PopularProductCard
-                  imgUrl={cake.imgUrl}
-                  imgAlt={cake.imgAlt}
-                  title={cake.title}
-                  description={cake.description}
-                  price={cake.price}
-                  redirectTo={cake.redirectTo}
-                />
-              </li>
+        {
+          error && (
+            <p>Error al cargar los resultados</p>
+          )
+        }
+        {
+          !error && (
+            <ul className="popular-products-container">
+              {
+                cakes.map(cake => (
+                  <li className="contents" key={cake.imgUrl}>
+                    <PopularProductCard
+                      imgUrl={cake.imgUrl}
+                      imgAlt={cake.imgAlt}
+                      title={cake.title}
+                      description={cake.description}
+                      price={cake.price}
+                      redirectTo={cake.redirectTo}
+                    />
+                  </li>
 
-            ))
-          }
-        </ul>
+                ))
+              }
+            </ul>
+          )
+        }
       </div>
     </section>
   )
